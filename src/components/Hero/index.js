16b371790e1dcfcb1edee38f719b265cea21de9f
zakir7dipu <zakir67@dipti.com.bd>
2022-03-13 16:50:22 +0600

    slider complete

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -10,12 +10,31 @@ import hero4 from '../../assets/images/shape/shape.png'
 import './style.css'
 import {Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 
 
 class Hero extends Component {
-    render() {
 
+    constructor() {
+        super();
+
+        this.state = {
+            basePath: "http://127.0.0.1:8000",
+            sliders: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`${this.state.basePath}/api/app/slider`)
+            .then(res => {
+                this.setState({sliders: res.data})
+            }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    render() {
         var settings = {
             dots: true,
             arrows: true,
@@ -30,70 +49,35 @@ class Hero extends Component {
         return (
             <section className="hero-slider hero-style-1">
                 <Slider {...settings}>
-                    <div className="slide">
-                        <div className="slide-inner" style={{ backgroundImage: `url(${hero1})`}}>
-                            <Container>
-                                <div className="slide-thumb">
-                                    <span>GO FOR HELP</span>
+                    {this.state.sliders.map(item => {
+                        return(
+                            <div className="slide" key={item.id}>
+                                <div className="slide-inner" style={{ backgroundImage: `url(${this.state.basePath+item.image})`}}>
+                                    <Container>
+                                        <div className="slide-thumb">
+                                            <span>{item.thumb}</span>
+                                        </div>
+                                        <div className="slide-title">
+                                            <h2>{item.title}</h2>
+                                        </div>
+                                        <div data-swiper-parallax="400" className="slide-text">
+                                            <p>{item.sub_title}</p>
+                                        </div>
+                                        {
+                                            item.btn_text?`<div  className="slide-btns">\\n' +
+                                                '                                            <Link to="${item.btn_url}" className="theme-btn">${item.btn_text}<i className="fa fa-angle-double-right" aria-hidden="true"></i></Link>\\n' +
+                                                '                                        </div>`:''
+                                        }
+
+                                    </Container>
+                                    <div className="slide-shape">
+                                        <img src={this.state.basePath+item.image} alt="" />
+                                    </div>
                                 </div>
-                                <div className="slide-title">
-                                    <h2>They Are Wait For Some Food.</h2>
-                                </div>
-                                <div data-swiper-parallax="400" className="slide-text">
-                                    <p>It is a long established fact that a reader will be distracted the readable content of a page when looking at its layout point of using of letters, as opposed.</p>
-                                </div>
-                                <div  className="slide-btns">
-                                    <Link to="/" className="theme-btn">Donate Now<i className="fa fa-angle-double-right" aria-hidden="true"></i></Link>
-                                </div>
-                            </Container>
-                            <div className="slide-shape">
-                                <img src={hero4} alt="" />
                             </div>
-                        </div>
-                    </div>
-                    <div className="slide">
-                        <div className="slide-inner" style={{ backgroundImage: `url(${hero2})`}}>
-                            <Container>
-                                <div className="slide-thumb">
-                                    <span>GO FOR HELP</span>
-                                </div>
-                                <div className="slide-title">
-                                    <h2>They Are Wait For Some Food.</h2>
-                                </div>
-                                <div data-swiper-parallax="400" className="slide-text">
-                                    <p>It is a long established fact that a reader will be distracted the readable content of a page when looking at its layout point of using of letters, as opposed.</p>
-                                </div>
-                                <div  className="slide-btns">
-                                    <Link to="/" className="theme-btn">Donate Now<i className="fa fa-angle-double-right" aria-hidden="true"></i></Link>
-                                </div>
-                            </Container>
-                            <div className="slide-shape">
-                               <img src={hero4} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="slide">
-                        <div className="slide-inner" style={{ backgroundImage: `url(${hero3})`}}>
-                            <Container>
-                                <div className="slide-thumb">
-                                    <span>GO FOR HELP</span>
-                                </div>
-                                <div className="slide-title">
-                                    <h2>They Are Wait For Some Food.</h2>
-                                </div>
-                                <div data-swiper-parallax="400" className="slide-text">
-                                    <p>It is a long established fact that a reader will be distracted the readable content of a page when looking at its layout point of using of letters, as opposed.</p>
-                                </div>
-                                <div  className="slide-btns">
-                                    <Link to="/donate" className="theme-btn">Donate Now<i className="fa fa-angle-double-right" aria-hidden="true"></i>
-                                    </Link>
-                                </div>
-                            </Container>
-                            <div className="slide-shape">
-                               <img src={hero4} alt="" />
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })}
+
                 </Slider>
             </section>
         )
